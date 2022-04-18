485b7367ae70b17e016c2ac3e070223332fa48e2
The Anonymous <abhikdas@Abhiks-MacBook-Air.local>
2022-04-19 01:00:48 +0530

    fix upload

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -6,148 +6,153 @@ import {createProduct, getAllCategories} from "./helper/adminapicall"
 
 
 
-export const AddProduct = () => {
-  const inputRef=useRef();
-  const{user,token}=isAuthenticated();
-  const formData=new FormData();
-        const [values, setValues] = useState({
-                name: "",
-                description: "",
-                price: "",
-                stock: "",
-                loading:false,
-                error:"",
-                createdProduct:"",
-                getaRedirect:false,
-              })
+export const AddProduct = () => {const{user,token}=isAuthenticated();
+const [values, setValues] = useState({
+        name: "",
+        description: "",
+        price: "",
+        stock: "",
+        loading:false,
+        error:"",
+        createdProduct:"",
+        getaRedirect:false,
+        formData:""
+      })
 
-              const { name, 
-                description, 
-                price,
-                stock,
-                loading,
-                error,
-                createdProduct,
-                getaRedirect,
-               } = values;
+      const { name, 
+        description, 
+        price,
+        stock,
+        loading,
+        error,
+        createdProduct,
+        getaRedirect,
+        formData
+       } = values;
+    const preload =()=>{
+      setValues({...values,formData:new FormData()});
+    }
+
+    useEffect(()=>{
+        preload();
+    },[])
 const onSubmit = (event) => {
-                event.preventDefault();
-                setValues({...values,error:"",loading:true});
-                createProduct(user._id,token,JSON.stringify(values))
-                .then(data=>{
-                  if(data.error){
-                    setValues({...values,error:data.error});
-                  }else{
-                    setValues({...values,
-                      name:"",
-                      description:"",
-                      price:"",
-                      photo:"",
-                      stock:"",
-                      createdProduct:data.name,
-                      loading:false});
-                  }
-                }).then(response=>{
-                   
-                })
-                .catch(err=>console.log(err));
-        //
-      };
-    
+        event.preventDefault();
+        setValues({...values,error:"",loading:true});
+        createProduct(user._id,token,formData)
+        .then(data=>{
+          if(data.error){
+            setValues({...values,error:data.error});
+          }else{
+            setValues({...values,
+              name:"",
+              description:"",
+              price:"",
+              photo:"",
+              stock:"",
+              createdProduct:data.name,
+              loading:false});
+          }
+        }).then(response=>{
+           
+        })
+        .catch(err=>console.log(err));
+//
+};
+
 const handleChange = name => event => {
-  const value=event.target.value;
-  formData.append(name,value)
-  setValues({...values,[name]:value});
-        //
+const value=name==="photo"?event.target.files[0]:event.target.value;
+formData.set(name,value);
+setValues({...values,[name]:value});
+//
 };
 
 const successMessage = () => (
-  <div className="alert alert-success mt-3" 
-  style={{display:createdProduct?"":"none"}}>
-    <h4>{createdProduct} created successfully</h4>
-  </div>
+<div className="alert alert-success mt-3" 
+style={{display:createdProduct?"":"none"}}>
+<h4>{createdProduct} created successfully</h4>
+</div>
 )
 const errorMessage = () => (
-  <div className="alert alert-danger mt-3" 
-  style={{display:error?"":"none"}}>
-    <h4>{error}</h4>
-  </div>
+<div className="alert alert-danger mt-3" 
+style={{display:error?"":"none"}}>
+<h4>{error}</h4>
+</div>
 )
 
-        const createProductForm = () => (
-                <form >
-                  <span>Post photo</span>
-                  <div className="form-group">
-                    <label className="btn btn-block btn-success">
-                      <input
-                        onChange={() => setValues({...values,photo:inputRef.current.files[0].name})}
-                        ref={inputRef}
-                        type="file"
-                        name="photo"
-                        accept="image"
-                        placeholder="choose a file"
-                      />
-                    </label>
-                  </div>
-                  <div className="form-group">
-                    <input
-                      onChange={handleChange("name")}
-                      name="photo"
-                      className="form-control"
-                      placeholder="Name"
-                      value={name}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <textarea
-                      onChange={handleChange("description")}
-                      name="photo"
-                      className="form-control"
-                      placeholder="Description"
-                      value={description}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      onChange={handleChange("price")}
-                      type="number"
-                      className="form-control"
-                      placeholder="Price"
-                      value={price}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      onChange={handleChange("stock")}
-                      type="number"
-                      className="form-control"
-                      placeholder="Quantity"
-                      value={stock}
-                    />
-                  </div>
-                  
-                  <button type="submit" onClick={onSubmit} className="btn btn-outline-success">
-                    Create Product
-                  </button>
-                </form>
-              );
+const createProductForm = () => (
+        <form >
+          <span>Post photo</span>
+          <div className="form-group">
+            <label className="btn btn-block btn-success">
+              <input
+                onChange={handleChange("photo")}
+                type="file"
+                name="photo"
+                accept="image"
+                placeholder="choose a file"
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("name")}
+              name="photo"
+              className="form-control"
+              placeholder="Name"
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              onChange={handleChange("description")}
+              name="photo"
+              className="form-control"
+              placeholder="Description"
+              value={description}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              onChange={handleChange("price")}
+              type="number"
+              className="form-control"
+              placeholder="Price"
+              value={price}
+            />
+          </div>
+             <div className="form-group">
+            <input
+              onChange={handleChange("stock")}
+              type="number"
+              className="form-control"
+              placeholder="Quantity"
+              value={stock}
+            />
+          </div>
+          
+          <button type="submit" onClick={onSubmit} className="btn btn-outline-success">
+            Create Product
+          </button>
+        </form>
+      );
 return(
-        <Base
-        title="Add Product"
-        description="create new products here"
-        className="container bg-dark p-4">
+<Base
+title="Add Product"
+description="create new products here"
+className="container bg-dark p-4">
 
 
-        <Link to="/admin/dashboard" className="btn btn-md btn-success md-3">Admin Home</Link>
-        <div className="row bg-light text-dark rounded">
-                <div className="col-md-8 offset-md-2">
-                  {successMessage()}
-                  {errorMessage()}
-                 {createProductForm()}
-                </div>
+<Link to="/admin/dashboard" className="btn btn-md btn-success md-3">Admin Home</Link>
+<div className="row bg-light text-dark rounded">
+        <div className="col-md-8 offset-md-2">
+          {successMessage()}
+          {errorMessage()}
+         {createProductForm()}
         </div>
+</div>
 
-        </Base>
+</Base>
 )
 }
 
